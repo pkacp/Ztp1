3529f091b7ec6fc05b113ddd85354ecbcc828af4
piotr <p.kacprzak.96@gmail.com>
2020-03-20 01:57:01 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,67 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author {Piotr Kacprzak}
+ * @version {1.0.0}
+ */
+
+public class Client {
+
+    /**
+     * Main function expecting to get filename as first argument
+     * and string pattern as second
+     * @param args
+     */
+    public static void main(String[] args) {
+        String fileName;
+        String pattern;
+        int bestMatchLineNumber;
+
+        try {
+            fileName = args[0];
+            pattern = args[1];
+            bestMatchLineNumber = findBestMatchingLineInFile(fileName, pattern);
+            System.out.println(bestMatchLineNumber);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Missing parameters");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Function calling Levenshtein algorithm for file with given name
+     * @param fileName
+     * @param pattern
+     * @return
+     */
+    private static int findBestMatchingLineInFile(String fileName, String pattern) {
+        Levenshtein levenshtein = new Levenshtein(pattern);
+
+        int lineCounter = 1;
+        int bestMatchLineNumber = 0;
+        int currentLineMatchValue;
+        int bestMatchValue = Integer.MAX_VALUE;
+
+        try {
+            FileInputStream inputStream = new FileInputStream(fileName);
+            Scanner myReader = new Scanner(inputStream);
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                currentLineMatchValue = levenshtein.compareWithPattern(line);
+                if (currentLineMatchValue < bestMatchValue) {
+                    bestMatchLineNumber = lineCounter;
+                    bestMatchValue = currentLineMatchValue;
+                }
+                if (bestMatchValue == 0) break;
+                lineCounter++;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+        return bestMatchLineNumber;
+    }
+}
---
+++ src/Levenshtein.java
@@ -0,0 +1,81 @@
+/**
+ * @author {Piotr Kacprzak}
+ * @version {1.0.0}
+ */
+
+public class Levenshtein {
+
+    private char[] pattern;
+
+    /**
+     * Constructor setting pattern as character array
+     * @param pattern
+     */
+    public Levenshtein(String pattern) {
+        this.pattern = pattern.toCharArray();
+    }
+
+    /**
+     * Function comparing passed String with pattern
+     * @param expressionString
+     * @return
+     */
+    public int compareWithPattern(String expressionString) {
+        char[] expression = expressionString.toCharArray();
+        return levenshteinImpl(expression);
+    }
+
+    /**
+     * Levenshtein distance algorithm implementation
+     * @param expression
+     * @return
+     */
+    private int levenshteinImpl(char[] expression) {
+        int[][] distanceMatrix = initMatrix(pattern.length, expression.length);
+        int cost;
+        for (int i = 1; i <= pattern.length; i++) {
+            for (int j = 1; j <= expression.length; j++) {
+                cost = calculateCost(pattern[i - 1], expression[j - 1]);
+                distanceMatrix[i][j] = pickMinimum(distanceMatrix[i - 1][j] + 1,
+                        distanceMatrix[i][j - 1] + 1,
+                        distanceMatrix[i - 1][j - 1] + cost);
+            }
+        }
+        return distanceMatrix[pattern.length][expression.length];
+    }
+
+    /**
+     * Matrix initialization for algorithm
+     * and setting firs col and row proper values
+     * @param matrixRows
+     * @param matrixCols
+     * @return
+     */
+    private int[][] initMatrix(int matrixRows, int matrixCols) {
+        int[][] matrix = new int[matrixRows + 1][matrixCols + 1];
+        for (int i = 0; i <= matrixRows; i++) matrix[i][0] = i;
+        for (int i = 1; i <= matrixCols; i++) matrix[0][i] = i;
+        return matrix;
+    }
+
+    /**
+     * Cost calculation 1 if values are different
+     * @param a
+     * @param b
+     * @return
+     */
+    private int calculateCost(int a, int b){
+        return a == b ? 0 : 1;
+    }
+
+    /**
+     * Picking minimum from three numbers
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
+    private int pickMinimum(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
